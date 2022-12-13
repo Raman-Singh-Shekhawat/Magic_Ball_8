7be0e13e5d82e58f5b3e9c697c35847fd72d5d1a
Raman-Singh-Shekhawat <rs8893@srmist.edu.in>
2022-12-13 15:33:41 +0530

    Project Added To GitHub

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,55 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      const MaterialApp(
-        home: null,
+      MaterialApp(home: BallPage()),
+    );
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Ask Me Anything'),
+        backgroundColor: Colors.blue[900],
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int imageNo = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  imageNo = Random().nextInt(5) + 1;
+                });
+                print('Button Pressed');
+              },
+              child: Image.asset('images/ball$imageNo.png'),
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
